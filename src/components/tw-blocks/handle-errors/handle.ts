1c397a40d1fe9664d19384e3cb0067ceaf101082
Joel Vargas <vermudo.com@gmail.com>
2025-08-27 14:13:06 -0600

    feat: implement error handling utilities with ApiErrorTypes enum and handleError function for improved API error management

---
+++ src/components/tw-blocks/handle-errors/handle.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { AxiosError } from "axios";
+import { ApiErrorTypes } from "./errors.enum";
+
+export type ErrorResponse = {
+  message: string;
+  code: number;
+  type: ApiErrorTypes;
+};
+
+export type WalletError = Pick<ErrorResponse, "message" | "code">;
+
+export const handleError = (error: AxiosError | WalletError): ErrorResponse => {
+  if (axios.isAxiosError(error)) {
+    const axiosError = error as AxiosError<ErrorResponse>;
+    const code = axiosError.response?.status || 500;
+    const message = axiosError.response?.data?.message || error.message;
+    return {
+      message,
+      code,
+      type: mapStatusCodeToErrorType(code),
+    };
+  } else if (error.code === -4) {
+    return {
+      message: "Wallet was closed before transaction was sent",
+      code: -4,
+      type: ApiErrorTypes.WALLET_ERROR,
+    };
+  } else {
+    return {
+      message: error.message,
+      code: 500,
+      type: ApiErrorTypes.UNKNOWN_ERROR,
+    };
+  }
+};
+
+const mapStatusCodeToErrorType = (code: number): ApiErrorTypes => {
+  switch (code) {
+    case 404:
+      return ApiErrorTypes.NOT_FOUND;
+    case 401:
+      return ApiErrorTypes.UNAUTHORIZED;
+    default:
+      return ApiErrorTypes.UNKNOWN_ERROR;
+  }
+};
