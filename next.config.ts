dd2893ecc4fdd9ed249f908c26e488c8a34d2311
Joel Vargas <vermudo.com@gmail.com>
2025-08-28 16:45:45 -0600

    feat: configure Next.js image settings to allow SVGs and define remote patterns for Google Drive images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "drive.usercontent.google.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
