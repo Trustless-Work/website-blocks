5e6b61d071738a056d00cc655c157bb120446321
Joel Vargas <vermudo.com@gmail.com>
2025-08-28 18:13:54 -0600

    feat: enhance GetStartedLayout with dynamic decorative SVG images and opacity adjustments for improved visual appeal

--- src/app/get-started/layout.tsx
+++ src/app/get-started/layout.tsx
@@ -1,22 +1,55 @@
+"use client";
+
 import type React from "react";
-import { SiteHeader } from "@/shared/SiteHeader";
 import { DocsSidebar } from "@/shared/DocsSidebar";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
 
 export default function GetStartedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  const decorativeOpacityClass = !mounted
+    ? "opacity-10"
+    : theme === "dark"
+      ? "opacity-10"
+      : "opacity-30";
   return (
-    <div className="min-h-screen">
-      <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-        <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
-          <DocsSidebar />
-        </aside>
-        <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
-          <div className="mx-auto w-full min-w-0">{children}</div>
-        </main>
+    <>
+      <Image
+        src="/design/wall.svg"
+        alt="Home"
+        width={1000}
+        height={1000}
+        className={`w-1/5 h-auto fixed -left-10 z-[-100] ${decorativeOpacityClass}`}
+        quality={100}
+      />
+
+      <Image
+        src="/design/wall.svg"
+        alt="Home"
+        width={1000}
+        height={1000}
+        className={`w-1/5 h-auto fixed -right-50 top-90 z-[-100] ${decorativeOpacityClass}`}
+        quality={100}
+      />
+      <div className="min-h-screen">
+        <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+          <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
+            <DocsSidebar />
+          </aside>
+          <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+            <div className="mx-auto w-full min-w-0">{children}</div>
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
