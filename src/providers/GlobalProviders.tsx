47c0be72e8aff7c4e40d849bedff4bdd12ab1c87
Joel Vargas <vermudo.com@gmail.com>
2025-09-04 09:43:33 -0600

    refactor: update import paths for Escrow providers to improve code organization and maintainability

--- src/providers/GlobalProviders.tsx
+++ src/providers/GlobalProviders.tsx
@@ -5,9 +5,9 @@ import { ClipboardProvider } from "@/providers/ClipboardProvider";
 import { ReactQueryClientProvider } from "@/components/tw-blocks/providers/ReactQueryClientProvider";
 import { TrustlessWorkProvider } from "@/components/tw-blocks/providers/TrustlessWork";
 import { WalletProvider } from "@/components/tw-blocks/wallet-kit/WalletProvider";
-import { EscrowProvider } from "@/components/tw-blocks/escrows/escrow-context/EscrowProvider";
-import { EscrowAmountProvider } from "@/components/tw-blocks/escrows/escrow-context/EscrowAmountProvider";
-import { EscrowDialogsProvider } from "@/components/tw-blocks/escrows/escrow-context/EscrowDialogsProvider";
+import { EscrowProvider } from "@/components/tw-blocks/providers/EscrowProvider";
+import { EscrowAmountProvider } from "@/components/tw-blocks/providers/EscrowAmountProvider";
+import { EscrowDialogsProvider } from "@/components/tw-blocks/providers/EscrowDialogsProvider";
 
 interface GlobalProvidersProps {
   children: React.ReactNode;
